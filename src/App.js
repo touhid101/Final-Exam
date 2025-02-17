f73c55c53cc6ec5478336c125e4e98cc4f0b312e
farhad <farhad.gsl21@gmail.com>
2025-02-17 12:15:54 +0600

    My Final Exam

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+	<h1>Hello, My Name is Md Touhidur Rahman!</h1>
+	<h1>Senior Software Engineer | Oracle Apex</h1>
+	<h1>Apex Holdings Limited</h1>
+        <p>Welcome to my React application.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
